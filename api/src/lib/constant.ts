57a78f7ff0f9e5b8c2c2d8193a95b07841f9c1a3
hbjs97 <hbjs97@naver.com>
2021-04-04 22:05:26 +0900

    etc, REG_EMAIL constant

--- api/src/lib/constant.ts
+++ api/src/lib/constant.ts
@@ -10,7 +10,7 @@ export const INTERNAL_ERROR = 500;
 
 export const REG_ENG_NUM = /^[a-zA-Z0-9]+$/;
 export const REG_ENG_NUM_KR = /^[a-zA-Z0-9\uac00-\ud7af]+$/;
-export const REG_MOBILE_PHONE = /^\d{3}-\d{3,4}-\d{4}$/;
+export const REG_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // text dataForm 입력과 선언된 텍스트에 맞는 파일을 받음
 // '-'만 특수문자 입력 가능
