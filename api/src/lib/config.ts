e88487b18eb40f4c3b9f69dd93e80c9552f2cb50
hbjs97 <hbjs97@naver.com>
2021-06-24 23:33:57 +0900

    etc, init s3 url

--- api/src/lib/config.ts
+++ api/src/lib/config.ts
@@ -37,6 +37,7 @@ export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || '';
 export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY || '';
 export const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION || '';
 export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME || '';
+export const AWS_S3_URL = process.env.AWS_S3_URL || '';
 
 export const S3 = new aws.S3({
   accessKeyId: AWS_ACCESS_KEY_ID,
